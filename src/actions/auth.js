fb0f6fd9fcc299dccb22a298860ff3e3d5fd9d96
Lucas Moyano <lucas.95moyano@gmail.com>
2021-04-25 21:22:14 -0300

    Mensajes de error

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,7 @@
 //thunk provee el dispatch para peticiones asincrobas
 //Acciones del login
+import Swal from 'sweetalert2';
+
 import { types } from '../types/types';
 import { firebase, googleAuthProvider } from '../firebase/firebase-config';
 import { finishLoading, startLoading } from './ui';
@@ -7,17 +9,19 @@ import { finishLoading, startLoading } from './ui';
 //Peticion asincrona del login
 export const startLoginEmailPassword = (email, password) => {
     return (dispatch) => {
+
         dispatch(startLoading());
+
         firebase.auth().signInWithEmailAndPassword( email, password )
             .then( ({ user }) => {
                 dispatch( login( user.uid, user.displayName ) );
+
+                dispatch(finishLoading());
             })
-            .catch( 
-                dispatch(finishLoading())
-            )
-            .finally(
-                dispatch(finishLoading())
-            )
+            .catch( e => {
+                dispatch(finishLoading());
+                Swal.fire('Error', e.message, 'error');
+            })
     }
 
 }
@@ -34,7 +38,9 @@ export const startRegisterWithEmailPasswordName = ( email, password, name ) => {
                     login( user.uid, user.displayName )
                 );
             })
-            .catch(e => console.log(e));
+            .catch(e => {
+                Swal.fire('Error', e.message, 'error');
+            });
     }
 }
 
